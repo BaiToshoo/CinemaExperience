83aef06ca9a5d5662a80ab4244e78ddb5bcd1381
BaiToshoo <toshetos04@gmail.com>
2024-03-01 01:06:32 +0200

    Created MovieController and it's first view and action

---
+++ CinemaExperience.Core/ViewModels/Movie/AllMoviesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaExperience.Core.ViewModels.Movie;
+public class AllMoviesViewModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Director { get; set; } = null!;
+    public string ImageUrl { get; set; } = null!;
+    
+}
---
+++ CinemaExperience/Controllers/MovieController.cs
@@ -0,0 +1,40 @@
+﻿using CinemaExperience.Core.ViewModels.Movie;
+using CinemaExperience.infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaExperience.Controllers;
+
+[Authorize]
+public class MovieController : Controller
+{
+    private readonly CinemaExperienceDbContext context;
+
+    public MovieController(CinemaExperienceDbContext dbContext)
+    {
+        context = dbContext;
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<IActionResult> All()
+    {
+        //Getting all of the movies from the database
+        var allMovies = await context.Movies
+            .AsNoTracking()
+            .Select(m => new AllMoviesViewModel()
+            {
+
+                Id = m.Id,
+                Title = m.Title,
+                Director = m.Director.Name,
+                ImageUrl = m.ImageUrl
+            })
+            .ToListAsync();
+
+        //Return all of the movies to the view
+        return View(allMovies);
+    }
+
+}
