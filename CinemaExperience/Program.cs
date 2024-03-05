baf31fa386028e8d5a18cdd9b2ed216087646efe
BaiToshoo <toshetos04@gmail.com>
2024-03-05 14:30:03 +0200

    Implemented decimal model binder

--- CinemaExperience/Program.cs
+++ CinemaExperience/Program.cs
@@ -1,10 +1,16 @@
+using CinemaExperience.ModelBinders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentityIdentity(builder.Configuration);
 builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options =>
+{
+	options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+});
+
 
 builder.Services.AddApplicationServices();
 
