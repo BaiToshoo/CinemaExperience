520e256b17489c6fd3872b2c432a03b700ca1f40
BaiToshoo <toshetos04@gmail.com>
2024-04-10 10:11:34 +0300

    UnitTest project added

---
+++ CinemaExperience.UnitTests/FilmUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace CinemaExperience.UnitTests;
+
+[TestFixture]
+public class FilmUnitTests
+{
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
