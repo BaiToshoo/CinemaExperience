fa8105c8c8b9bd5631ade2c51de9939b24138db9
BaiToshoo <toshetos04@gmail.com>
2024-04-12 15:06:06 +0300

    Edited login and register pages

--- CinemaExperience/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CinemaExperience/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -109,6 +109,7 @@ namespace CinemaExperience.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("UserFullNameClaim", $"{user.FirstName} {user.LastName}"));
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
